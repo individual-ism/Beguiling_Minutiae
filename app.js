4ba188afaaab56378910fab25056f4ffe197f63b
individual-ism <alexlchang@icloud.com>
2022-12-01 15:07:30 -0500

    still fixing - semi-functional

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ const questionAnswerPairs = [
     answer: 'silvio berlusconi',
     points: 3},
     {number: 4,
-    question: 'Moonlight Sonata is one of the most well-known works of Ludwig van Beethoven, but few recall the opus in its official name. Provide the opus number to Piano Sonata No. 14 in Arabic numerals.',
-    answer: 27,
+    question: 'Moonlight Sonata is one of the most well-known works of Ludwig van Beethoven, but few recall the opus in its official name. Provide the opus number to Piano Sonata No. 14 in words.',
+    answer: 'twenty-seven',
     points: 5},
     {number: 5,
     question: `Immanuel Kant, the Seventeenth Century German philosopher, famously wrote a collection of three philosophical critiques, with The Critique of Pure Reason and The Critique of Practical Reason being far more recognized. What was the subject matter of The Critique of Judgment? Choices: ${q5.innerHTML}`,
@@ -39,113 +39,126 @@ const questionAnswerPairs = [
     {number: 6,
     question: 'Each number represents a letter in the English alphabet. What is the full phrase of the abbreviation represented by XVII V IV?',
     answer: 'quod erat demonstrandum',
-    points: 5},
-    {number: 7,
-    question: ''}
+    points: 5}
 ];
 
 let playerAnswer = document.querySelector('#answer');
-let fullQuestion = document.querySelector('.questionFullText');
+let fullQuestion = document.querySelector('.question');
 let declarations = document.querySelector('.declarations');
 let endGame = document.querySelector('#endgame');
 let scoreboard = document.querySelector('.scoreboard');
 let playAgain = document.querySelector('#reset');
-let questionPosed = document.querySelector('.questionFullText');
+let questionPosed = document.querySelector('.question');
 let submitButton = document.querySelector('#submit');
 let nextQButton = document.querySelector('#nextQuestion');
+let startGame = document.querySelector('.startGame')
 let player1Score = 0;
 let player2Score = 0;
-let player = 'Player 1';
+let player;
 let utilizedQuestions = [];
 let primaryTurn;
-// let questionIndex = Math.floor(Math.random() * questionAnswerPairs.length);
+let questionIndex = Math.floor(Math.random() * questionAnswerPairs.length);
+let playerTurn = document.querySelector('.playerTurn');
+let lcPlayerAnswer = playerAnswer.innerHTML.toLowerCase();
+let submittingButton = document.querySelector('#submitting');
+let subBu = document.querySelector('#subBu');
 
 
 
-primaryTurn ? 'Player 1' : 'Player 2';
-declarations.innerHTML = `${player}, here is your challenge for contemplation`;
+primaryTurn ? player = 'Player 1' : player = 'Player 2';
+playerTurn.innerHTML = `${player}, here is your challenge for contemplation`;
+
+
 
 function selectQuestion() {
     let questionIndex = Math.floor(Math.random() * questionAnswerPairs.length);
     questionPosed.innerHTML = questionAnswerPairs[questionIndex].question;
-    // return questionPosed.innerHTML;
-}
+};
 
 function evaluateAnswer() {
-    if (playerAnswer.innerHTML.toLowerCase() === questionAnswerPairs[questionIndex].answer) {
-        if (primaryTurn === true) {
+    // let questionIndex = Math.floor(Math.random() * questionAnswerPairs.length);
+    // selectQuestion();
+    // while (player1Score < 150 && player2Score < 150) {
+    if (lcPlayerAnswer === questionAnswerPairs[questionIndex].answer) {
+        if (player === 'Player 1') {
             player1Score += questionAnswerPairs[questionIndex].points;
-        } else if (primaryTurn = false) {
+            primaryTurn = false;
+            player = 'Player 2';
+        } else if (player = 'Player 2') {
             player2Score += questionAnswerPairs[questionIndex].points;
-        }
-    } else {
-        declarations.innerHTML = 'Unaccepted answer';
-        if (primaryTurn === true) {
+            primaryTurn = true;
+            player = 'Player 1';
+        };
+    } else if (lcPlayerAnswer !== questionAnswerPairs[questionIndex].answer) {
+        declarations.innerHTML = 'Incorrect';
+        if (player === 'Player 1') {
             player1Score -= Math.ceil(questionAnswerPairs[questionIndex].points / 2);
-        } else if (primaryTurn = false) {
+            primaryTurn = false;
+            player = 'Player 2';
+        } else if (player === 'Player 2') {
             player2Score -= Math.ceil(questionAnswerPairs[questionIndex].points / 2);
-        }
-    }
+            primaryTurn = true;
+            player = 'Player 1';
+        };
+    };
+    playerTurn.innerHTML = `${player}, here is your challenge for contemplation`;
+    utilizedQuestions.push(questionPosed);
+    questionAnswerPairs.slice(questionAnswerPairs[questionIndex]);
     scoreboard.innerHTML = `Player 1 Points: ${player1Score} | Player 2 Points: ${player2Score}`;
-}
+};
+
 
 function winCondition() {
     let conditions = [player1Score > player2Score, player2Score > player1Score];
     conditions.forEach(condition => {
         if (conditions[0]) {
             declarations.innerHTML = 'Congratulations, Player 1. You reign victorious.';
-            // return declarations.innerHTML;
         } else if (conditions[1]) {
             declarations.innerHTML = 'Kudos to you, Player 2, on your success this game.';
-            // return declarations.innerHTML;
         };
     });
-}
+};
 
 function conclude() {
     if (player1Score === player2Score) {
         declarations.innerHTML = 'You are too evenly matched in intellect. Till next your minds compete, pursue your curiosity, lest it kill your cat.';
-        // return declarations.innerHTML;
     } else {
         winCondition();
-    }
-}
+    };
+};
 
 function reset() {
     player1Score = 0;
     player2Score = 0;
-    // playerAnswer.innerHTML.reset();
-    primaryTurn = true;
-    document.getElementByClass('scoreboard').reset();
-}
+    playerAnswer.reset();
+    // player = 'Player 1';
+    scoreboard.innerHTML = `Player 1 Points: ${player1Score} | Player 2 Points: ${player2Score}`;
+};
+
+
+
+function playGame() {
+    while (player1Score < 150 && player2Score < 150) {
+        selectQuestion();
+        evaluateAnswer();
+        playerAnswer.reset();
+        if (player === 'Player 1') {
+            player = 'Player 2';
+        } else {
+            player = 'Player 1';
+        };
+    };
+    conclude();
+};
 
 nextQButton.addEventListener('click', selectQuestion);
 submitButton.addEventListener('click', evaluateAnswer);
 playAgain.addEventListener('click', reset);
 endGame.addEventListener('click', conclude);
-
-function playGame() {
-    while (player1Score < 150 && player2Score < 150){
-        selectQuestion();
-        evaluateAnswer();
-        playerAnswer.innerHTML = '';
-        primaryTurn ? player = 'Player 1' : player = 'Player 2'
-    }
-        conclude();
-}
-
+startGame.addEventListener('click', selectQuestion);
+// submittingButton.addEventListener('click', evaluateAnswer);
 
 
 // playGame();
 
-// function playingGame() {
-//     while (player1Score < 150 && player2Score < 150){
-//         selectQuestion();
-//         evaluateAnswer();
-//     }
-//     isDraw();
-//     winCondition();
-// }
-
-// playingGame();
-
+// console.log(lcPlayerAnswer);
