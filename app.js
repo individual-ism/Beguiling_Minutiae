fedfbfbed04b3842f980e07c6e9e866fa777a78c
individual-ism <alexlchang@icloud.com>
2022-12-02 14:01:03 -0500

    most up-to-date commit - partial functionality

--- app.js
+++ app.js
@@ -1,46 +1,101 @@
-// Setting base for multiple choice answers
-class multipleChoiceOptions {
-    constructor(a, b, c, d, e) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-        this.d = d;
-        this.e = e;
+// // Setting base for multiple choice answers
+// class multipleChoiceOptions {
+//     constructor(a, b, c, d, e) {
+//         this.a = a;
+//         this.b = b;
+//         this.c = c;
+//         this.d = d;
+//         this.e = e;
+//     }
+// }
+
+// // // Defining multiple choice answers
+// const q3 = new multipleChoiceOptions('Matteo Salvini', 'Mario Draghi', 'Matteo Renzi', 'Silvio Berlusconi', 'Carlo Azeglio Ciampi');
+// const q5 = new multipleChoiceOptions('Meta Ethics', 'Aesthetics', 'Asceticism', 'Religion', 'Practical Ethics');
+// const q7 = new multipleChoiceOptions('Isomorphic', 'Catamorphic', 'Anthropomorphic', )
+
+// // Listing all question/answer pairs with respective numbers and point values
+// const questionAnswerPairs = [
+//     {number: 1,
+//     question: 'In declaring that "American blood has been shed on American soil", President James Polk sought congressional approval to retaliate against British-controlled Canada.',
+//     answer: 'false',
+//     points: 1},
+//     {number: 2,
+//     question: 'After President Woodrow Wilson fell ill in the latter half of his second term, this person is rumored to have taken unofficial control of the presidency. What is the first and last name of this person?',
+//     answer: 'edith wilson',
+//     points: 5},
+//     {number: 3,
+//     question: `Italy recently elected and installed its most right-wing government since Mussolini. This new coalition government, led by Giorgia Meloni, includes support from which former prime minister? Choices (Type full name in the response box): ${q3.innerHTML}`,
+//     answer: 'silvio berlusconi',
+//     points: 3},
+//     {number: 4,
+//     question: 'Moonlight Sonata is one of the most well-known works of Ludwig van Beethoven, but few recall the opus in its official name. Provide the opus number to Piano Sonata No. 14 in words.',
+//     answer: 'twenty-seven',
+//     points: 5},
+//     {number: 5,
+//     question: `Immanuel Kant, the Seventeenth Century German philosopher, famously wrote a collection of three philosophical critiques, with The Critique of Pure Reason and The Critique of Practical Reason being far more recognized. What was the subject matter of The Critique of Judgment? Choices: ${q5.innerHTML}`,
+//     answer: 'aesthetics',
+//     points: 3},
+//     {number: 6,
+//     question: 'Each number represents a letter in the English alphabet. What is the full phrase of the abbreviation represented by XVII V IV?',
+//     answer: 'quod erat demonstrandum',
+//     points: 5}
+// ];
+
+// class qNa {
+//     constructor(number, question, answer, points) {
+//         this.number = number;
+//         this.question = question;
+//         this.answer = answer;
+//         this.points = points;
+//     }
+// }
+
+// const zero = new qNa(1, 'In declaring that "American blood has been shed on American soil", President James Polk sought congressional approval to retaliate against British-controlled Canada.', 'false', 1);
+// const one = new qNa(2, 'After President Woodrow Wilson fell ill in the latter half of his second term, this person is rumored to have taken unofficial control of the presidency. What is the first and last name of this person?', 'edith wilson', 5);
+// const two = new qNa(3, 'Italy recently elected and installed its most right-wing government since Mussolini. This new coalition government, led by Giorgia Meloni, includes support from which former prime minister?', 'silvio berlusconi', 3);
+// const three = new qNa(4, 'Moonlight Sonata is one of the most well-known works of Ludwig van Beethoven, but few recall the opus in its official name. Provide the opus number to Piano Sonata No. 14 in words.', 'twenty-seven', 5);
+// const four = (5, 'Immanuel Kant, the Seventeenth Century German philosopher, famously wrote a collection of three philosophical critiques, with The Critique of Pure Reason and The Critique of Practical Reason being far more recognized. What was the subject matter of The Critique of Judgment?', 'aesthetics', 5);
+// const five = (6, 'Each number represents a letter in the English alphabet. What is the full phrase of the abbreviation represented by XVII V IV?', 'quod erat demonstrandum', 5);
+
+const qA = [
+    {
+        number: 0,
+        question: 'In declaring that "American blood has been shed on American soil", President James Polk sought congressional approval to retaliate against British-controlled Canada.',
+        answer: 'false',
+        points: 1},
+    {
+        number: 1,
+        question: 'After President Woodrow Wilson fell ill in the latter half of his second term, this person is rumored to have taken unofficial control of the presidency. What is the first and last name of this person?',
+        answer: 'edith wilson',
+        points: 5
+    },
+    {
+        number: 2,
+        question: 'Italy recently elected and installed its most right-wing government since Mussolini. This new coalition government, led by Giorgia Meloni, includes support from which former prime minister?',
+        answer: 'silvio berlusconi',
+        points: 3
+    },
+    {
+        number: 3,
+        question: 'Moonlight Sonata is one of the most well-known works of Ludwig van Beethoven, but few recall the opus in its official name. Provide the opus number to Piano Sonata No. 14 in words.',
+        answer: 'twenty-seven',
+        points: 5
+    },
+    {
+        number: 4,
+        question: 'Immanuel Kant, the Seventeenth Century German philosopher, famously wrote a collection of three philosophical critiques, with The Critique of Pure Reason and The Critique of Practical Reason being far more recognized. What was the subject matter of The Critique of Judgment?',
+        answer: 'aesthetics',
+        points: 3
+    },
+    {
+        number: 5,
+        question: 'Each number represents a letter in the English alphabet. What is the full phrase of the abbreviation represented by XVII V IV?',
+        answer: 'quod erat demonstrandum',
+        points: 5
     }
-}
-
-// Defining multiple choice answers
-const q3 = new multipleChoiceOptions('Matteo Salvini', 'Mario Draghi', 'Matteo Renzi', 'Silvio Berlusconi', 'Carlo Azeglio Ciampi');
-const q5 = new multipleChoiceOptions('Meta Ethics', 'Aesthetics', 'Asceticism', 'Religion', 'Practical Ethics');
-const q7 = new multipleChoiceOptions('Isomorphic', 'Catamorphic', 'Anthropomorphic', )
-
-// Listing all question/answer pairs with respective numbers and point values
-const questionAnswerPairs = [
-    {number: 1,
-    question: 'In declaring that "American blood has been shed on American soil", President James Polk sought congressional approval to retaliate against British-controlled Canada.',
-    answer: 'false',
-    points: 1},
-    {number: 2,
-    question: 'After President Woodrow Wilson fell ill in the latter half of his second term, this person is rumored to have taken unofficial control of the presidency. What is the first and last name of this person?',
-    answer: 'edith wilson',
-    points: 5},
-    {number: 3,
-    question: `Italy recently elected and installed its most right-wing government since Mussolini. This new coalition government, led by Giorgia Meloni, includes support from which former prime minister? Choices (Type full name in the response box): ${q3.innerHTML}`,
-    answer: 'silvio berlusconi',
-    points: 3},
-    {number: 4,
-    question: 'Moonlight Sonata is one of the most well-known works of Ludwig van Beethoven, but few recall the opus in its official name. Provide the opus number to Piano Sonata No. 14 in words.',
-    answer: 'twenty-seven',
-    points: 5},
-    {number: 5,
-    question: `Immanuel Kant, the Seventeenth Century German philosopher, famously wrote a collection of three philosophical critiques, with The Critique of Pure Reason and The Critique of Practical Reason being far more recognized. What was the subject matter of The Critique of Judgment? Choices: ${q5.innerHTML}`,
-    answer: 'aesthetics',
-    points: 3},
-    {number: 6,
-    question: 'Each number represents a letter in the English alphabet. What is the full phrase of the abbreviation represented by XVII V IV?',
-    answer: 'quod erat demonstrandum',
-    points: 5}
 ];
+
 
 let playerAnswer = document.querySelector('#answer');
 let fullQuestion = document.querySelector('.question');
@@ -56,12 +111,10 @@ let player1Score = 0;
 let player2Score = 0;
 let player;
 let utilizedQuestions = [];
+let userInput = playerAnswer.innerHTML.toLowerCase();
 let primaryTurn;
-let questionIndex = Math.floor(Math.random() * questionAnswerPairs.length);
+let indexNum = Math.floor(Math.random() * qA.length);
 let playerTurn = document.querySelector('.playerTurn');
-let lcPlayerAnswer = playerAnswer.innerHTML.toLowerCase();
-let submittingButton = document.querySelector('#submitting');
-let subBu = document.querySelector('#subBu');
 
 
 
@@ -71,39 +124,39 @@ playerTurn.innerHTML = `${player}, here is your challenge for contemplation`;
 
 
 function selectQuestion() {
-    let questionIndex = Math.floor(Math.random() * questionAnswerPairs.length);
-    questionPosed.innerHTML = questionAnswerPairs[questionIndex].question;
+    qA.forEach(q => {
+        let indexNum = Math.floor(Math.random() * qA.length);
+        questionPosed.innerHTML = qA[indexNum].question;
+    })
 };
 
 function evaluateAnswer() {
-    // let questionIndex = Math.floor(Math.random() * questionAnswerPairs.length);
-    // selectQuestion();
-    // while (player1Score < 150 && player2Score < 150) {
-    if (lcPlayerAnswer === questionAnswerPairs[questionIndex].answer) {
+    // userInput = playerAnswer.innerHTML.toLowerCase();
+    if (userInput === qA[indexNum].answer) {
         if (player === 'Player 1') {
-            player1Score += questionAnswerPairs[questionIndex].points;
+            player1Score += qA[indexNum].points;
             primaryTurn = false;
             player = 'Player 2';
         } else if (player = 'Player 2') {
-            player2Score += questionAnswerPairs[questionIndex].points;
+            player2Score += qA[indexNum].points;
             primaryTurn = true;
             player = 'Player 1';
         };
-    } else if (lcPlayerAnswer !== questionAnswerPairs[questionIndex].answer) {
+    } else if (userInput !== qA[indexNum].answer) {
         declarations.innerHTML = 'Incorrect';
         if (player === 'Player 1') {
-            player1Score -= Math.ceil(questionAnswerPairs[questionIndex].points / 2);
+            player1Score -= Math.ceil(qA[indexNum].points / 2);
             primaryTurn = false;
             player = 'Player 2';
         } else if (player === 'Player 2') {
-            player2Score -= Math.ceil(questionAnswerPairs[questionIndex].points / 2);
+            player2Score -= Math.ceil(qA[indexNum].points / 2);
             primaryTurn = true;
             player = 'Player 1';
         };
     };
     playerTurn.innerHTML = `${player}, here is your challenge for contemplation`;
-    utilizedQuestions.push(questionPosed);
-    questionAnswerPairs.slice(questionAnswerPairs[questionIndex]);
+    // utilizedQuestions.push(questionPosed);
+    // qA.slice(qA[indexNum], 1);
     scoreboard.innerHTML = `Player 1 Points: ${player1Score} | Player 2 Points: ${player2Score}`;
 };
 
@@ -155,10 +208,13 @@ nextQButton.addEventListener('click', selectQuestion);
 submitButton.addEventListener('click', evaluateAnswer);
 playAgain.addEventListener('click', reset);
 endGame.addEventListener('click', conclude);
-startGame.addEventListener('click', selectQuestion);
-// submittingButton.addEventListener('click', evaluateAnswer);
+startGame.addEventListener('click', () => {
+    selectQuestion();
+    startGame.removeEventListener('click', selectQuestion);
+});
 
 
 // playGame();
-
-// console.log(lcPlayerAnswer);
+console.log(qA[indexNum].question);
+console.log(qA[indexNum].answer);
+console.log(playerAnswer.innerHTML);
